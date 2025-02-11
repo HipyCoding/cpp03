6cda737888b77128f18840fcdbe827e6269df0ff
Candrese <candrese@student.42Heilbronn.de>
2025-02-11 12:17:01 +0100

    fragtrap class with its member functions seems to work fine

---
+++ ex02/FragTrap.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 07:10:19 by candrese          #+#    #+#             */
+/*   Updated: 2025/02/11 12:09:40 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+#include <iostream>
+
+// Default constructor
+FragTrap::FragTrap() : ClapTrap() {
+	std::cout << "FragTrap default constructor called" << std::endl;
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+}
+
+// Parameterized constructor
+FragTrap::FragTrap(std::string& name) : ClapTrap(name) {
+	std::cout << "FragTrap parameterized constructor called" << std::endl;
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+}
+
+// Copy constructor
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other) {
+	std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+FragTrap& FragTrap::operator=(const FragTrap& other) {
+	std::cout << "FragTrap copy assignment operator called" << std::endl;
+	if (this != &other) {
+		ClapTrap::operator=(other);
+	}
+	return *this;
+}
+
+// Destructor
+FragTrap::~FragTrap() {
+	std::cout << "FragTrap destructor called for " << _name << std::endl;
+}
+
+void FragTrap::highFivesGuys(void) {
+	if (_hitPoints == 0) {
+		std::cout << "FragTrap " << _name << " doesn't have enough hp for a high five :(" 
+		<< std::endl;
+		return;
+	}
+		std::cout << "FragTrap " << _name << " requests a high five!" << std::endl;
+}
---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 07:10:16 by candrese          #+#    #+#             */
+/*   Updated: 2025/02/11 11:53:58 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <iostream>
+# include "ClapTrap.hpp"
+
+class	FragTrap : public ClapTrap {
+	public:
+		// Orthodox Canonical Form
+		FragTrap();
+		FragTrap(std::string& name);
+		FragTrap(const FragTrap& other);
+		FragTrap&	operator=(const FragTrap& other);
+		~FragTrap();
+
+		// Memberfunctions
+		void highFivesGuys(void);
+};
+
+#endif
