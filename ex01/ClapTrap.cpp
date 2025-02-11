d403b72697d4aca21bfb5f3870ebe6945f096ff9
Candrese <candrese@student.42Heilbronn.de>
2025-02-11 08:36:28 +0100

    clpatrap implementation pretty much from last exercise

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 23:00:17 by christian         #+#    #+#             */
+/*   Updated: 2025/02/11 08:23:06 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+// Default constructor
+ClapTrap::ClapTrap() : 
+	_name("unnamed"), 
+	_hitPoints(10), 
+	_energyPoints(10), 
+	_attackDamage(0) 
+{
+	std::cout << "ClapTrap default constructor called" << std::endl;
+}
+
+// Parameterized constructor
+ClapTrap::ClapTrap(std::string_view name) : 
+	_name(name), 
+	_hitPoints(10), 
+	_energyPoints(10), 
+	_attackDamage(0) 
+{
+	std::cout << "ClapTrap parameterized constructor called for " << _name << std::endl;
+}
+
+// Copy constructor
+ClapTrap::ClapTrap(const ClapTrap& other) : 
+	_name(other._name),
+	_hitPoints(other._hitPoints),
+	_energyPoints(other._energyPoints),
+	_attackDamage(other._attackDamage)
+{
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
+	std::cout << "ClapTrap copy assignment operator called" << std::endl;
+	if (this != &other) {
+		_name = other._name;
+		_hitPoints = other._hitPoints;
+		_energyPoints = other._energyPoints;
+		_attackDamage = other._attackDamage;
+	}
+	return *this;
+}
+
+// Destructor
+ClapTrap::~ClapTrap() {
+	std::cout << "ClapTrap destructor called for " << _name << std::endl;
+}
+
+void ClapTrap::attack(std::string_view target) {
+	if (_energyPoints == 0 || _hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " can't attack - no energy or hit points left!" << std::endl;
+		return;
+	}
+	
+	_energyPoints--;
+	std::cout << "ClapTrap " << _name << " attacks " << target 
+		<< ", causing " << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	if (_hitPoints <= amount) {
+		_hitPoints = 0;
+	} else {
+		_hitPoints -= amount;
+	}
+	
+	std::cout << "ClapTrap " << _name << " takes " << amount 
+		<< " points of damage! Remaining HP: " << _hitPoints << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	if (_energyPoints == 0 || _hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " can't repair - no energy or hit points left!" << std::endl;
+		return;
+	}
+	
+	_energyPoints--;
+	_hitPoints += amount;
+	std::cout << "ClapTrap " << _name << " repairs itself for " << amount 
+		<< " points! Current HP: " << _hitPoints << std::endl;
+}
+
+void	ClapTrap::displayStatus() const
+{
+	std::cout << "ScavTrap " << _name <<  " HP: " << _hitPoints << ", Energy: " << _energyPoints
+		<< ", Attack Damage: " << _attackDamage << "\n";
+}
---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 23:00:20 by christian         #+#    #+#             */
+/*   Updated: 2025/02/11 07:38:39 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <string_view>
+
+class ClapTrap
+{
+	protected:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+
+	public:
+		// Orthodox Canonical Form
+		ClapTrap();
+		explicit ClapTrap(std::string_view name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+
+		// Member functions
+		void attack(std::string_view target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+		// getter Member functions
+		void	displayStatus() const;
+};
+
+#endif
