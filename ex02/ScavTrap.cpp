1d6a5b4c71add117ded7eb7bceeff07069988f48
Candrese <candrese@student.42Heilbronn.de>
2025-02-11 12:15:28 +0100

    ScavTrap class with its member functions from previous exercise

---
+++ ex02/ScavTrap.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 04:42:11 by candrese          #+#    #+#             */
+/*   Updated: 2025/02/11 08:04:02 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+// Default constructor
+ScavTrap::ScavTrap() : ClapTrap("unnamed") {
+	std::cout << "ScavTrap default constructor called" << std::endl;
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+}
+
+// Parameterized constructor
+ScavTrap::ScavTrap(std::string& name) : ClapTrap(name) {
+	std::cout << "ScavTrap parameterized constructor called for " << _name << std::endl;
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+}
+
+// Copy constructor
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other) {
+	std::cout << "ScavTrap copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+ScavTrap& ScavTrap::operator=(const ScavTrap& other) {
+	std::cout << "ScavTrap copy assignment operator called" << std::endl;
+	if (this != &other) {
+		_name = other._name;
+		_hitPoints = other._hitPoints;
+		_energyPoints = other._energyPoints;
+		_attackDamage = other._attackDamage;
+	}
+	return *this;
+}
+
+// Destructor
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap destructor called for " << _name << std::endl;
+}
+
+// Attack function
+void ScavTrap::attack(const std::string& target) {
+	if (_energyPoints == 0 || _hitPoints == 0) {
+		std::cout << "ScavTrap " << _name << " can't attack - no energy or hit points left!" << std::endl;
+		return;
+	}
+	_energyPoints--;
+	std::cout << "ScavTrap " << _name << " attacks " << target
+		<< ", causing " << _attackDamage << " points of damage!" << std::endl;
+}
+
+// Guard Gate mode
+void ScavTrap::guardGate() {
+	std::cout << "ScavTrap " << _name << " is now guarding the gate!" << std::endl;
+}
---
+++ ex02/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 04:42:14 by candrese          #+#    #+#             */
+/*   Updated: 2025/02/11 12:10:46 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_H
+# define SCAVTRAP_H
+
+# include "ClapTrap.hpp"
+
+class	ScavTrap : public ClapTrap {
+	public:
+		// Orthodox Canonical Form
+		ScavTrap();
+		ScavTrap(std::string& name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap&	operator=(const ScavTrap& other);
+		~ScavTrap();
+
+		// Memberfunctions
+		void 		attack(const std::string& target);
+		void 		guardGate(void);
+};
+
+#endif
